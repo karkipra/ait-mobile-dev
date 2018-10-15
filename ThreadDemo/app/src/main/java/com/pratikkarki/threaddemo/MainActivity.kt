859ab3eab0c7860250cca2848e3dfa82ff1f555c
Pratik Karki <karkipra@grinnell.edu>
2018-10-15 11:52:29 +0200

    thread demo

--- ThreadDemo/app/src/main/java/com/pratikkarki/threaddemo/MainActivity.kt
+++ ThreadDemo/app/src/main/java/com/pratikkarki/threaddemo/MainActivity.kt
@@ -3,21 +3,59 @@ package com.pratikkarki.threaddemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var enabled = false
+
     private inner class MyAppendThread : Thread(){
         override fun run(){
-            tvData.append("#")
+            while(enabled){
+                runOnUiThread{
+                    tvData.append("#")
+                }
+                sleep(1000)
+            }
         }
     }
+
+    private inner class MyTimerTask : TimerTask() {
+        override fun run() {
+            runOnUiThread {
+                tvTime.text = Date().toString()
+            }
+
+        }
+    }
+
+    private lateinit var mainTimer : Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainTimer = Timer()
+
         btnStart.setOnClickListener{
-            MyAppendThread().start()
+            if(!enabled) {
+                enabled = true
+                MyAppendThread().start()
+            }
         }
+
+        btnStop.setOnClickListener(){
+            enabled = false
+        }
+
+        btnStartTimer.setOnClickListener{
+            mainTimer.schedule(MyTimerTask(), 0 , 1000)
+        }
+    }
+
+    override fun onStop() {
+        enabled = false
+        mainTimer.cancel()
+        super.onStop()
     }
 }
